0b90997130ae0f58e0ecf984ede78a2db4bb564a
Michał Rogaliński <Micha Rogaliski>
2016-10-29 22:05:05 +0200

    launch configurations

--- src/extension.ts
+++ src/extension.ts
@@ -7,37 +7,70 @@
 import * as vscode from 'vscode';
 
 const initialConfigurations = {
-	version: '0.2.0',
-	configurations: [
+	configuration1: [
 	{
-		name: 'Bash-Debug',
+		name: 'Bash-Debug (select script from list of sh files)',
 		type: 'bashdb',
 		request: 'launch',
-		program: '${workspaceRoot}/${command.AskForProgramName}',
+		program: '${workspaceRoot}/${command.SelectScriptName}',
+		stopOnEntry: true
+	}],
+	configuration2: [
+	{
+		name: 'Bash-Debug (hardcoded script name)',
+		type: 'bashdb',
+		request: 'launch',
+		program: '${workspaceRoot}/path/to/script.sh',
+		stopOnEntry: true
+	}],
+	configuration3: [
+	{
+		name: 'Bash-Debug (type in script name)',
+		type: 'bashdb',
+		request: 'launch',
+		program: '${workspaceRoot}/${command.AskForScriptName}',
 		stopOnEntry: true
 	}
-]}
+	]}
 
 export function activate(context: vscode.ExtensionContext) {
 
-	let disposable = vscode.commands.registerCommand('extension.getProgramName', () => {
+	let disposable1 = vscode.commands.registerCommand('extension.getProgramName', () => {
 		return vscode.window.showInputBox({
 			placeHolder: "Please enter the relative path to bash script.",
 			value: "./path/to/script.sh"
 		});
 	});
-	context.subscriptions.push(disposable);
+	context.subscriptions.push(disposable1);
 
-	// TODO: list all bash scripts instead of uncomfortable input
-	// let disposable2 = vscode.commands.registerCommand('extension.getProgramName', () => {
-	// 	return vscode.window.showQuickPick(
-	// 		["path1", "path2"]
-	// 	);
-	// });
-	// context.subscriptions.push(disposable2);
+	let disposable2 = vscode.commands.registerCommand('extension.selectProgramName', () => {
+
+		return vscode.workspace.findFiles("**/*.sh", "").then((uris) => {
+			var list = new Array<string>();
+			for (var i = 0 ; i < uris.length ; i++){
+				list.push(uris[i].fsPath);
+			}
+			return vscode.window.showQuickPick(list);
+		});
+	});
+	context.subscriptions.push(disposable2);
 
 	context.subscriptions.push(vscode.commands.registerCommand('extension.bash-debug.provideInitialConfigurations', () => {
-		return JSON.stringify(initialConfigurations);
+		return vscode.window.showQuickPick(
+			["1. Script path should be selected from drop-down list of shell scripts in workspace",
+			"2. Script path should be hardcoded in launch task",
+			"3. Script path should be typed in by developer when launching"
+		]).then((result)=>{
+			switch(parseInt(result.substr(0,1)))
+			{
+				case 1:
+					return JSON.stringify(initialConfigurations.configuration1);
+				case 2:
+					return JSON.stringify(initialConfigurations.configuration2);
+				default:
+					return JSON.stringify(initialConfigurations.configuration3);
+			}
+		})
 	}));
 }
 
