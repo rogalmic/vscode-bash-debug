b215c0339a09898a0781e4bddee214d3278ffeb4
Michal <rogalinsky@gmail.com>
2018-09-14 22:54:45 +0200

    chore: small fixes

--- bashdb_dir/lib/filecache.sh
+++ bashdb_dir/lib/filecache.sh
@@ -201,14 +201,14 @@ function _Dbg_readin {
     typeset source_array
     typeset -ri NOT_SMALLFILE=1000
 
-    if [[ -z $filename ]] || [[ $filename == "$_Dbg_bogus_file" ]] ; then
-	eval "${_Dbg_source_array_var}[0]=\"$Dbg_EXECUTION_STRING\""
-  else
+    if [[ -z "$filename" ]] || [[ "$filename" == "$_Dbg_bogus_file" ]] ; then
+	  eval "${_Dbg_source_array_var}[0]=\"$Dbg_EXECUTION_STRING\""
+    else
 	fullname=$(_Dbg_resolve_expand_filename "$filename")
-	if [[ -r $fullname ]] ; then
+	if [[ -r "$fullname" ]] ; then
 	    typeset -r progress_prefix="Reading $filename"
-	    _Dbg_file2canonic[$filename]="$fullname"
-	    _Dbg_file2canonic[$fullname]="$fullname"
+	    _Dbg_file2canonic["$filename"]="$fullname"
+	    _Dbg_file2canonic["$fullname"]="$fullname"
 	    # Use readarray which speeds up reading greatly.
 	    typeset -ri BIGFILE=30000
 	    if wc -l < /dev/null >/dev/null 2>&1 ; then
@@ -235,7 +235,7 @@ function _Dbg_readin {
 		[[ -r "$tempfile" ]] && rm "$tempfile"
 	    fi
 
-	    (( line_count > BIGFILE)) && _Dbg_progess_done "\n"
+	    (( line_count > BIGFILE)) && _Dbg_progess_done "" || _Dbg_msg ""
 	else
 	    return 1
 	fi
